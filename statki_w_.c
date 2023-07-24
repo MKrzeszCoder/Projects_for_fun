dbafd36ea95faa9f2e0fb08411f370071db274f6
MKrzeszCoder <140010541+MKrzeszCoder@users.noreply.github.com>
2023-07-24 19:59:54 +0200

    Update statki_w_.c
    Gra w Statki w C
    Projekt ten to klasyczna gra w statki, napisana w języku C.
    Funkcje
    Wyświetlanie planszy: Gra posiada interfejs tekstowy, który wyraźnie pokazuje planszę gry, włącznie z miejscami, w które strzelono.
    Interaktywna rozgrywka: Gra pozwala na strzelanie do statków przeciwnika, zaznaczając trafione i chybione strzały na planszy.
    Komunikaty o trafieniach: Gra informuje gracza o trafieniach w statki przeciwnika.
    Wygrana: Gra wyświetla komunikat, gdy gracz wygrywa, pokazując jego końcowy wynik.
    Podgląd planszy przeciwnika: Na koniec gry, gracz może zobaczyć, gdzie znajdowały się statki przeciwnika, co pozwala na analizę strategii i planowanie przyszłych gier.
    Wnioski:
    Ten projekt to doskonały przykład na to, jak można stworzyć prostą, ale wciągającą grę w języku C. Jest to świetna baza do dalszego rozwoju, na przykład dodania graficznego interfejsu użytkownika, rozbudowania mechaniki gry, czy implementacji gry wieloosobowej.
    ## w przyszłości planuje dodać pomiar czasu gry w ilu strzałach udało się wygrać oraz możliwość gry dla wielu osób

--- statki_w_.c
+++ statki_w_.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include "time.h"
+#include <time.h>
 #include <stdlib.h>
+
 #define N 10
 #define M 10
 
@@ -9,10 +10,8 @@ int main(){
     srand(time(NULL));
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            tablica[i][j] = rand() % 1;
-            if(tablica[i][j]==1){
-                break;
-            }
+            tablica[i][j] = 0;
+            
         }
     }
 
@@ -22,14 +21,17 @@ int main(){
     while (placed < 10) {
         x=rand()%M;
         y=rand()%N;
+        if(tablica[y][x]==1)
+            continue;
         tablica[y][x]=1;
         placed++;
     }
 
     int statki=10;
     int liczba,liczba2;
+    printf("musisz ustrzelic 10 malych statkow, powodzenia!\n");
     while(statki>0){
-        printf("Podaj jakie pole strzelasz kolumny a potem rzedy ");
+        printf("Podaj jakie pole strzelasz kolumny a potem rzedy:");
         scanf("%d %d",&liczba,&liczba2);
         while(getchar()!='\n');
         if(tablica[liczba][liczba2]==1)
@@ -43,20 +45,32 @@ int main(){
         if(tablica[liczba][liczba2]==0){
             tablica[liczba][liczba2]=3;
         }
+        else if (tablica[liczba][liczba2] == 3) {
+            printf("Oj w to pole juz strzelales!\n");
+            continue;
+        }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(tablica[i][j]==3){
-                    printf(" 0");
-                }
-                if(tablica[i][j]==2){
+                 if (tablica[i][j] == 0 || tablica[i][j] == 1)
+                    printf(" -");
+                else if (tablica[i][j] == 2)
                     printf(" X");
-                }
-                else if (tablica[i][j]==1||tablica[i][j]==0)printf(" -");
+                else
+                    printf(" 0");
             }
             printf("\n");
         }
 
     }
 
-
+printf("WOW wygrales gratulacje!\n");
+    for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+               if (tablica[i][j] == 2)
+                printf(" X");
+            else
+                printf(" -");
+            }
+            printf("\n");
+        }
 }
